06d8864476f7fdc788083c920d6699be005c9dde
AgentTroll <woodyc40@gmail.com>
2014-02-14 19:23:34 -0800

    Delete MIUClient.java

--- src/MIUClient.java
+++
@@ -1,7 +0,0 @@
-import netio.ConnectionFactory;
-
-public class MIUClient {
-    public static void main(String[] args) throws Throwable {
-        ConnectionFactory.newConnection("127.0.0.1");
-    }
-}
